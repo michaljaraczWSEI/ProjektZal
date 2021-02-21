0a77d5ecd0d4e2d854de832a594105550b0bf54d
m.jaracz <m.jaracz@sharespace.work>
2021-02-21 01:03:10 +0100

    feature: connect WPF app with SQLite, feature: write reusable style in App.xaml, feature: create Home and Report pages

---
+++ DataBaseService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Dapper;
+using System.Data;
+using System.Data.SQLite;
+
+namespace ExpenseIt
+{
+    public class DataBaseService
+    {
+        private string LoadConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+        }
+
+        public List<PersonModel> LoadUsers()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                return cnn.Query<PersonModel>("select * from Person", new DynamicParameters()).ToList();
+            };
+        }
+        public void SavePerson(PersonModel person)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into Person (FirstName, LastName) values (@FirstName, @LastName, @Department)", person);
+            };
+        }
+    }
+}
